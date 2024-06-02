142b78133eaa00e479132438303de57ed44b5864
DESKTOP-2FH8J2L\houzi <houzidalao91@gmail.com>
2024-06-02 17:23:37 -0400

    got index.html to render

--- resume_website/resume_website_backend/resume_website_backend/settings.py
+++ resume_website/resume_website_backend/resume_website_backend/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))#Path(__file__).resolve().parent.parent
+BUILD_DIR = os.path.join(BASE_DIR, 'resume_website_frontend', 'build')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'resume_website_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'resume_website_frontend/build')],
+        'DIRS': [os.path.join(BUILD_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'resume_website_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'resume_website_frontend/build/static')]
+STATICFILES_DIRS = [os.path.join(BUILD_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- resume_website/resume_website_backend/resume_website_backend/views.py
+++ resume_website/resume_website_backend/resume_website_backend/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+def base(request):
+    return render(request, 'build/base.html')
+
 def index(request):
-    return render(request, 'index.html')
+    firstName = "Ryan"
+    lastName = "Giannamore"
+    context = {
+        "first": firstName,
+        "last": lastName,
+    }
+    return render(request, 'build/index.html', context)
+
