bd2ca5b0c799525e4d53dab5d540fb2c42566190
DESKTOP-2FH8J2L\houzi <houzidalao91@gmail.com>
2024-06-03 14:42:20 -0400

    created models.py and initialized all of the model classes.

---
+++ resume_website/resume_website_backend/resume_website_backend/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Job(models.Model):
+    pass
+
+class Education(models.Model):
+    pass
+
+class Project(models.Model):
+    pass
+
+class Skill(models.Model):
+    pass
+
+class Certificate(models.Moedl):
+    pass
