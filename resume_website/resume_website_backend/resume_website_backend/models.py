d9cd9e8d2c89909246aad932e99667bb51a141c4
DESKTOP-2FH8J2L\houzi <houzidalao91@gmail.com>
2024-06-03 15:15:38 -0400

    Created the fields for each model

--- resume_website/resume_website_backend/resume_website_backend/models.py
+++ resume_website/resume_website_backend/resume_website_backend/models.py
@@ -1,16 +1,31 @@
 from django.db import models
 
 class Job(models.Model):
-    pass
+    job_title = models.CharField(max_length=50)
+    company = models.CharField(max_length=50)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    duration = models.DurationField(blank=True, null=True)
+    description = models.TextField()
 
 class Education(models.Model):
-    pass
+    school = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    duration = models.DurationField(blank=True, null=True)
+    description = models.TextField()
 
 class Project(models.Model):
-    pass
+    project_name = models.CharField(max_length=50)
+    org = models.CharField(max_length=50)
+    description = models.TextField()
 
 class Skill(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    description = models.TextField()
 
 class Certificate(models.Moedl):
-    pass
+    name = models.CharField(max_length=50)
+    school = models.CharField(max_length=50)
+    link = models.URLField(max_length=500)
