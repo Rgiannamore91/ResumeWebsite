4ad32c3f1b4ea986b8edd66531dd74086674972d
DESKTOP-2FH8J2L\houzi <houzidalao91@gmail.com>
2024-06-05 19:51:47 -0400

    Created the forgotten summary model, added functionality to the Job and Education models that calculate duration.

--- resume_website/resume_website_backend/resume_website_backend/models.py
+++ resume_website/resume_website_backend/resume_website_backend/models.py
@@ -1,32 +1,72 @@
 from django.db import models
+from datetime import timedelta, datetime
+
+class Summary(models.Model):
+    body = models.TextField()
 
 class Job(models.Model):
     job_title = models.CharField(max_length=50)
     company = models.CharField(max_length=50)
     from_date = models.DateField()
-    to_date = models.DateField()
-    duration = models.DurationField(blank=True, null=True)
+    to_date = models.DateField(blank=True, null=True)
+    duration = models.DurationField(editable=False, blank=True, null=True)
     description = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if self.to_date is None:
+            self.duration = datetime.now() - self.from_date
+        else:
+            self.duration = self.to_date - self.from_date
+
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ["from_date"]
 
     def __str__(self):
-        return  f"Job Title: {self.job_title}, Company: {self.company}"            
+        return  f"Job Title: {self.job_title}, Company: {self.company}"  
+    
+    @property
+    def is_ongoing(self):
+        return self.to_date is None  
+
+    @property
+    def duration_calculated(self):
+        if self.to_date:
+            return self.to_date - self.from_date
+        return datetime.now() - self.from_date       
 
 class Education(models.Model):
     school = models.CharField(max_length=50)
     major = models.CharField(max_length=50)
     from_date = models.DateField()
-    to_date = models.DateField()
-    duration = models.DurationField(blank=True, null=True)
+    to_date = models.DateField(blank=True, null=True)
+    duration = models.DurationField(editable=False, blank=True, null=True)
     description = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if self.to_date is None:
+            self.duration = datetime.now() - self.from_date
+        else:
+            self.duration = self.to_date - self.from_date
+
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ["from_date"]
 
     def __str__(self):
         return f"School: {self.school}, Major: {self.major}"
+    
+    @property
+    def is_ongoing(self):
+        return self.to_date is None  
+
+    @property
+    def duration_calculated(self):
+        if self.to_date:
+            return self.to_date - self.from_date
+        return datetime.now() - self.from_date    
     
 class Project(models.Model):
     project_name = models.CharField(max_length=50)
